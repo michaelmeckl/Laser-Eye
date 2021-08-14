500d789253e6a24ea9a92ab3cfbebeed9ed626cf
michaelmeckl <michael.meckl@stud.uni-regensburg.de>
2021-08-14 18:50:38 +0200

    Comment out broken pipeline for now

--- machine_learning_predictor/run_ml_pipeline.py
+++ machine_learning_predictor/run_ml_pipeline.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
-# from preprocess_image_data_batch_version import start_preprocessing
 from preprocess_image_data import start_preprocessing
-from cnn_test import train_model
+# from cnn_test import train_model
 
 
 def main():
     # preprocess data
     start_preprocessing()
     # train a machine learning model
-    train_model()
+    # train_model()  # TODO doesn't work right now; file needs refactoring to work in pipeline
 
 
 if __name__ == "__main__":
