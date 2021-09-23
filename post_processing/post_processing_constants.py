800642e7e21e5d07467282f1b54672b639f08b6b
michaelmeckl <michael.meckl@stud.uni-regensburg.de>
2021-09-23 19:15:50 +0200

    Fix path in post processing constants

--- post_processing/post_processing_constants.py
+++ post_processing/post_processing_constants.py
@@ -1,3 +1,5 @@
+import os
+
 download_folder = "tracking_data_download"
 image_folder = "extracted_images"
 logs_folder = "extracted_logs"
@@ -6,4 +8,4 @@ post_processing_log_folder = "post_processing_data"
 blur_threshold = 25  # determined by testing with different participants
 
 evaluation_study_folder = "evaluation_study"
-evaluation_download_folder = "evaluation_study_data"
+evaluation_download_folder = os.path.join(os.path.dirname(__file__), evaluation_study_folder, "evaluation_study_data")
